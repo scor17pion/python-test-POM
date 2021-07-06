884d12a6b88aa02cd3efc14ea1d33ad24f3ae7b5
scor17pion <61062721+scor17pion@users.noreply.github.com>
2021-07-06 19:17:17 +0300

    first commit

---
+++ test_main_page.py
@@ -0,0 +1,9 @@
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link.click()
+   
+# pytest.exe -s -v --tb=line --language=en D:\w_py\test_main_page.py
+# cd C:\Users\scor1\AppData\Local\Programs\Python\Python38\Scripts\
+# C:\Users\scor1\AppData\Local\Programs\Python\Python38\Scripts\pytest.exe -s -v --tb=line --language=en test_main_page.py
