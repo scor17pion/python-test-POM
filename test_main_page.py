0d79580c40777dd42285891fd7bbbcd5409892d9
scor17pion <scor17pion@yandex.ru>
2021-07-07 07:55:41 +0300

    Update test_main_page.py
    add comment with command "cd d:\Alex\Docs\GitHub\python-test-POM\"

--- test_main_page.py
+++ test_main_page.py
@@ -6,4 +6,5 @@ def test_guest_can_go_to_login_page(browser):
    
 # pytest.exe -s -v --tb=line --language=en D:\w_py\test_main_page.py
 # cd C:\Users\scor1\AppData\Local\Programs\Python\Python38\Scripts\
+# cd d:\Alex\Docs\GitHub\python-test-POM\
 # C:\Users\scor1\AppData\Local\Programs\Python\Python38\Scripts\pytest.exe -s -v --tb=line --language=en test_main_page.py
