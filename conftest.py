d22b616edd0df45f0d99f841b242c29828af5649
scor17pion <scor17pion@yandex.ru>
2021-07-07 12:02:04 +0300

    add tests for login page
    add test for login page

--- conftest.py
+++ conftest.py
@@ -16,6 +16,7 @@ def browser(request):
         print("\nstart chrome browser for test..")
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language}) 
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
