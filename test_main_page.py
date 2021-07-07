75687b960fa49266d5e767d9e98bf54231811848
scor17pion <scor17pion@yandex.ru>
2021-07-07 08:20:33 +0300

    update 2 files
    updare 2 files: main_page.py, test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,7 @@
+from pages.main_page import MainPage
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
-   
-# pytest.exe -s -v --tb=line --language=en D:\w_py\test_main_page.py
-# cd C:\Users\scor1\AppData\Local\Programs\Python\Python38\Scripts\
-# cd d:\Alex\Docs\GitHub\python-test-POM\
-# C:\Users\scor1\AppData\Local\Programs\Python\Python38\Scripts\pytest.exe -s -v --tb=line --language=en test_main_page.py
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
